0485bcaaadb231c1bcc37517d3f29cfe97e35cfe
Jaehoo Im <im_jaehoo@kakao.com>
2021-01-06 02:55:53 +0900

    chore: remove console.log

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -40,7 +40,7 @@ const App = ({
   const loginButtonClickHandler = () => {
     setIsAuthModalOpen(true);
   };
-  console.log(process.env);
+
   useEffect(() => {
     if (!currentUser) {
       setRecommendationCriterion('random');
