0fc7bcb63b73093b8003fa1b9daa0a03996f4a33
Jaehoo Im <im_jaehoo@kakao.com>
2021-01-09 09:09:18 +0900

    chore: change env variable name

--- src/api/fetchRecommendationSymbolList.js
+++ src/api/fetchRecommendationSymbolList.js
@@ -4,7 +4,7 @@ import PATHS from '../constants/paths';
 const fetchRecommendationSymbolList = async keyword => {
   const serverRoot
     = process.env.NODE_ENV === 'development'
-      ? process.env.REACT_APP_LOCALHOST
+      ? process.env.REACT_APP_SERVER_LOCALHOST
       : process.env.REACT_APP_PROD_SERVER_ROOT;
 
   const response = await fetch(
